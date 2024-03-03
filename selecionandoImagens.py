b131bc7e86b43efdcccc2c3c5fe3c03777060c23
AthuloSantana <athulosantana@gmail.com>
2024-03-03 17:51:30 -0300

    Criar função para retornar imagens em grade

--- selecionandoImagens.py
+++ selecionandoImagens.py
@@ -15,6 +15,27 @@ imagens_escolhidas = random.sample(arquivos_de_imagem, quantidade_de_imagens)
 
 caminhosImagens = [os.path.join(diretorio, imagem) for imagem in imagens_escolhidas]
 
+def criar_grid():
+    imagens = []
+
+    imagens = caminhosImagens * 2
+    random.shuffle(imagens)
+
+    grid = []
+
+    for _ in range(4):
+        linhas = []
+
+        for _ in range(4):
+            imagem = imagens.pop()
+            linhas.append(imagem)
+
+        grid.append(linhas)
+
+    return grid
+
+imagens = criar_grid()
+
 # Testando
 # for caminho in caminhosImagens:
 #     print(caminho)
